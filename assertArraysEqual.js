a7c16f5bc62a6b9f4166bb6bb77fb670d58aafc5
Mustafa <mustafa31112@gmail.com>
2022-01-12 08:46:23 +0000

    loging a message

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function (a, b) {
+  if (a.length !== b.length) {
+    return false 
+  }
+  for (let i = 0; i < a.length; i++) {
+   if (a[i] !== b[i]) {
+     return false
+   }
+
+  }
+return true;
+}
+
+const assertArraysEqual = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+    } else {
+      console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+    }
+ 
+   }
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+
