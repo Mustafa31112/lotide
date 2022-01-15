c65085c7be02f51d10d5f435fdd66ce03aea3807
Mustafa <mustafa31112@gmail.com>
2022-01-15 19:23:01 +0000

    return taking Until function

---
+++ takeUntil.js
@@ -0,0 +1,35 @@
+const takeUntil = function(array, callback) {
+  let newArray = [];
+  for (let element of array) {
+    if (!callback(element)) {
+      newArray.push(element);
+    } else {
+      break;
+    }
+  }
+  return newArray;
+  };
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
+const assertArraysEqual = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+    } else {
+      console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+    }
+ 
+   }
+}
+
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2])
