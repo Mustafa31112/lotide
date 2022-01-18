158fb926a0245e681dda81d19f3d8ebd48bb3bc9
Mustafa <mustafa31112@gmail.com>
2022-01-18 01:55:51 +0000

    added chai tests and removed old test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,17 @@
-const eqArrays = require('../eqArrays')
-const assertArraysEqual = require('../assertArraysEqual');
+
 const middle = require('../middle')
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+
+  it("returns 2 and 3 for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]); 
+  });
+});
 
 
 
@@ -10,10 +21,3 @@ const middle = require('../middle')
 
 
 
-
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-console.log(middle([1, 2, 3]))// => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
